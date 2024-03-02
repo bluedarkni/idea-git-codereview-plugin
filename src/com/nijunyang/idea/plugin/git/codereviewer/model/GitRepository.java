443879fa39f65267b0349e1e6638ecab37c67c9f
bluedarkni <bluedarkni@qq.com>
2024-03-02 08:45:17 +0000

    update src/com/nijunyang/idea/plugin/git/codereviewer/model/GitRepository.java.
    Signed-off-by: bluedarkni <bluedarkni@qq.com>

--- src/com/nijunyang/idea/plugin/git/codereviewer/model/GitRepository.java
+++ src/com/nijunyang/idea/plugin/git/codereviewer/model/GitRepository.java
@@ -4,7 +4,7 @@ package com.nijunyang.idea.plugin.git.codereviewer.model;
  * Description:
  * Created by nijunyang on 2022/1/28 14:27
  */
-public abstract class GitProject {
+public abstract class GitRepository {
 
     public abstract Integer getId();
 }
