473f8a54a3c21a87f1f826dba798dbb8dfe3c7fe
bluedarkni <bluedarkni@qq.com>
2024-03-02 08:43:54 +0000

    update src/com/nijunyang/idea/plugin/git/codereviewer/model/gitlab/GitLabRepository.java.
    Signed-off-by: bluedarkni <bluedarkni@qq.com>

--- src/com/nijunyang/idea/plugin/git/codereviewer/model/gitlab/GitLabRepository.java
+++ src/com/nijunyang/idea/plugin/git/codereviewer/model/gitlab/GitLabRepository.java
@@ -1,16 +1,19 @@
 package com.nijunyang.idea.plugin.git.codereviewer.model.gitlab;
 
-import com.nijunyang.idea.plugin.git.codereviewer.model.GitProject;
+import com.nijunyang.idea.plugin.git.codereviewer.model.GitRepository;
 
 /**
  * Description:
  * Created by nijunyang on 2022/1/28 11:46
  */
-public class GitLabProject extends GitProject {
+public class GitLabRepository extends GitRepository {
 
     private Integer id;
     private String description;
     private String name;
+    private String ssh_url_to_repo;
+    private String http_url_to_repo;
+    private String web_url;
 
     public Integer getId() {
         return id;
@@ -34,5 +37,30 @@ public class GitLabProject extends GitProject {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+
+    public String getSsh_url_to_repo() {
+        return ssh_url_to_repo;
+    }
+
+    public void setSsh_url_to_repo(String ssh_url_to_repo) {
+        this.ssh_url_to_repo = ssh_url_to_repo;
+    }
+
+    public String getHttp_url_to_repo() {
+        return http_url_to_repo;
+    }
+
+    public void setHttp_url_to_repo(String http_url_to_repo) {
+        this.http_url_to_repo = http_url_to_repo;
+    }
+
+    public String getWeb_url() {
+        return web_url;
+    }
+
+    public void setWeb_url(String web_url) {
+        this.web_url = web_url;
     }
 }
