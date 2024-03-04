5f01447daeaa043102dfca9d93a593041aa426e9
bluedarkni <bluedarkni@qq.com>
2024-03-04 07:16:46 +0000

    add src/com/nijunyang/idea/plugin/git/codereviewer/model/gitee/GiteeRepository.java.
    Signed-off-by: bluedarkni <bluedarkni@qq.com>

---
+++ src/com/nijunyang/idea/plugin/git/codereviewer/model/gitee/GiteeRepository.java
@@ -0,0 +1,100 @@
+package com.nijunyang.idea.plugin.git.codereviewer.model.gitee;
+
+import com.nijunyang.idea.plugin.git.codereviewer.model.GitRepository;
+
+/**
+ * Description:
+ * Created by GW00295392 on 2024/3/4
+ */
+public class GiteeRepository extends GitRepository {
+
+    private Integer id;
+    private String full_name;
+    private String human_name;
+    private String url;
+
+    private String path;
+    private String name;
+    private String description;
+
+    private String html_url;
+    private String ssh_url;
+
+
+
+
+
+
+    @Override
+    public Integer getId() {
+        return this.id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getFull_name() {
+        return full_name;
+    }
+
+    public void setFull_name(String full_name) {
+        this.full_name = full_name;
+    }
+
+    public String getHuman_name() {
+        return human_name;
+    }
+
+    public void setHuman_name(String human_name) {
+        this.human_name = human_name;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getHtml_url() {
+        return html_url;
+    }
+
+    public void setHtml_url(String html_url) {
+        this.html_url = html_url;
+    }
+
+    public String getSsh_url() {
+        return ssh_url;
+    }
+
+    public void setSsh_url(String ssh_url) {
+        this.ssh_url = ssh_url;
+    }
+}
