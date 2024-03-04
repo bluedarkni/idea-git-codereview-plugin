4c4a73c95c1feea2d49f7e8ded4ff4568ba07d45
bluedarkni <bluedarkni@qq.com>
2024-03-04 08:12:56 +0000

    update src/com/nijunyang/idea/plugin/git/codereviewer/utils/PathUtil.java.
    Signed-off-by: bluedarkni <bluedarkni@qq.com>

--- src/com/nijunyang/idea/plugin/git/codereviewer/utils/PathUtil.java
+++ src/com/nijunyang/idea/plugin/git/codereviewer/utils/PathUtil.java
@@ -2,6 +2,7 @@ package com.nijunyang.idea.plugin.git.codereviewer.utils;
 
 import cn.hutool.core.io.FileUtil;
 import com.intellij.openapi.project.Project;
+import com.nijunyang.idea.plugin.git.codereviewer.model.LocalRepositoryInfo;
 
 import java.io.File;
 import java.nio.charset.Charset;
@@ -20,17 +21,50 @@ public final class PathUtil {
      * 从项目.git目录下config文件中获取项目地址，以便解析域名和项目名
      * @return
      */
-    public static String loadProjectUrl(Project project) {
+    public static LocalRepositoryInfo localRepositoryInfo(Project project) {
         String projectPath = project.getBasePath();
         String configPath = PathUtil.combinePath(projectPath, ".git", "config");
         List<String> stringList = FileUtil.readLines(configPath, Charset.defaultCharset());
+
         for (String msg : stringList) {
             if (msg.trim().startsWith("url")) {
                 String[] split = msg.split("=");
-                return split[split.length - 1].trim();
+                String url = split[split.length - 1].trim();
+
+                LocalRepositoryInfo localRepositoryInfo = new LocalRepositoryInfo();
+                localRepositoryInfo.setUrl(url);
+                localRepositoryInfo.setName(project.getName());
+                //https url  和 ssh url
+                if (url.startsWith(HttpUtil.HTTPS_PROTOCOL)) {
+                    String domain = UrlUtil.parseDomainName(url);
+                    localRepositoryInfo.setDomain(domain);
+
+                    String[] split1 = url.split("domain");
+                    int lastIndex = split1[1].lastIndexOf("/");
+                    localRepositoryInfo.setGroupPath(split1[1].substring(1, lastIndex));
+                } else if (url.startsWith("git@")) {
+                    String[] split1 = url.split("@");
+                    String[] split2 = split1[1].split(":");
+                    localRepositoryInfo.setDomain(split2[0]);
+                    int lastIndex = split2[1].lastIndexOf("/");
+                    localRepositoryInfo.setGroupPath(split2[1].substring(0, lastIndex));
+                }
+                return localRepositoryInfo;
             }
         }
+
         throw new RuntimeException("not find git config");
+    }
+
+    public static void main(String[] args) {
+        String url = "https://gitlab.btpoc.com/platform/commercialize-loop/rights-package/bt-rightspackage-service.git";
+        String[] split1 = url.split("gitlab.btpoc.com");
+        System.out.println(split1[1]);
+
+        int lastIndex = split1[1].lastIndexOf("/");
+        String result = split1[1].substring(1, lastIndex);
+        System.out.println(result);
+
     }
 
     public static String combinePath(String...args){
