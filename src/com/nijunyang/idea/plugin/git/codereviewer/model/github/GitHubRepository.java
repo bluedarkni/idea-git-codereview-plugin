5edbc2dba934e769d9bfa4935c9c2f38282065d4
bluedarkni <bluedarkni@qq.com>
2024-03-05 07:32:42 +0000

    add src/com/nijunyang/idea/plugin/git/codereviewer/model/github/GitHubRepository.java.
    Signed-off-by: bluedarkni <bluedarkni@qq.com>

---
+++ src/com/nijunyang/idea/plugin/git/codereviewer/model/github/GitHubRepository.java
@@ -0,0 +1,16 @@
+package com.nijunyang.idea.plugin.git.codereviewer.model.github;
+
+import com.nijunyang.idea.plugin.git.codereviewer.model.GitRepository;
+
+/**
+ * Description:
+ * Created by GW00295392 on 2024/3/5
+ */
+public class GitHubRepository extends GitRepository {
+
+
+    @Override
+    public Integer getId() {
+        return null;
+    }
+}
